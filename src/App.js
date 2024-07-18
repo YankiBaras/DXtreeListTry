480c3dc23f431ac83f06eec9965b1a2e2c875f21
Yanki Baras <yanki@ras.land>
2024-07-18 12:33:16 +0300

    for learning the dxtreelist in depth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import 'devextreme/dist/css/dx.light.css';
+
+import {
+    TreeList,
+    Column,
+    ColumnFixing,
+    ColumnChooser,
+    FilterRow,
+    SearchPanel, 
+    RequiredRule,
+    Editing
+} from 'devextreme-react/tree-list';
+import { employees } from './employees';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <TreeList
+            dataSource={employees}
+
+                columnAutoWidth={true}
+                allowColumnResizing={true}
+                allowColumnReordering={true}
+                rootValue={-1}
+                keyExpr="ID"
+                parentIdExpr="HeadID">
+                <FilterRow visible={true} />
+                <SearchPanel visible={true} />
+                <Column dataField='FullName' > <RequiredRule /></Column>
+                <Column dataField="Email" visible={false} />
+                <ColumnChooser enabled={true} />
+                <Column dataField='Position'><RequiredRule /></Column>
+                <Column dataField='BirthDate'
+                    dataType='date'
+                    width={400}>
+                </Column>
+                <Column dataField='HireDate'
+                    dataType='date' width={100}><RequiredRule /></Column>
+                <Column dataField='City' />
+                <Column dataField='State' />
+                <Column dataField='Email' />
+                <Column dataField='MobilePhone' />
+                <Column dataField='Skype' />
+                <Editing
+                    mode="popup"
+                    allowUpdating={true}
+                    allowDeleting={true}
+                    allowAdding={true}
+                />
+            </TreeList>
+        </div>
+    );
 }
 
 export default App;
